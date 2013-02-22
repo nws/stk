b9ab36c191315e9c6af360c3118f93512a1ee33d
huf <huf@keszen.hu>
2013-02-22 17:22:34 +0100

    added skeleton init shellscript

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cp stk/run.php.example run.php
+chmod 755 run.php
+
+cp stk/index.php.example index.php
+
+mkdir cfg
+
+mkdir -p smarty/compile
+chmod 777 smarty/compile
+
+mkdir tiles
+mkdir tiles/www
+mkdir tiles/scripts
+mkdir -p tmpl/www
+
