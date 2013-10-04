2b754b8c591f229aac66ebe684a40ed51277c71d
huf <huf@keszen.hu>
2013-10-04 19:53:10 +0200

    updated lib/fc, this one can do ttls

--- lib/fc.php
+++ lib/fc.php
@@ -1,12 +1,27 @@
 <?php
 
 class fc {
-	static function get($store, $key) {
+	static $force_regen = false;
+
+	static function force_regen($v) {
+		self::$force_regen = $v;
+	}
+
+	static function get($store, $key, $ttl = null) {
 		if (!config::$fcache_dir) {
+			return;
+		}
+		if (self::$force_regen) {
 			return;
 		}
 		if ($content = @file_get_contents(self::filename($store, $key))) {
 			if ($decoded = @unserialize($content)) {
+				if ($ttl !== null) {
+					$age = time() - $decoded['time'];
+					if ($age > $ttl) {
+						return;
+					}
+				}
 				return $decoded['value'];
 			}
 		}
@@ -17,16 +32,6 @@ class fc {
 			return;
 		}
 		$fn = self::filename($store, $key);
-		$wrap = array(
-			'value' => $value,
-			'time' => time(),
-		);
-		file_put_contents($fn, serialize($wrap));
-	}
-
-	static function filename($store, $key) {
-		$fn = config::$fcache_dir.$store.'/'.md5(json_encode($key)).'.s';
-
 		$dir = dirname($fn);
 		if (!is_dir($dir)) {
 			if (!is_dir(config::$fcache_dir)) {
@@ -36,7 +41,30 @@ class fc {
 			@mkdir($dir);
 			@chmod($dir, 0777);
 		}
+		$wrap = array(
+			'value' => $value,
+			'time' => time(),
+		);
+		$tempfn = $fn.'.temp';
+		$r = @file_put_contents($tempfn, serialize($wrap));
+		if ($r===false) {
+			debug('fc::put unable to write '.$fn);
+		}
+		else {
+			@rename($tempfn, $fn);
+		}
+		return $r;
+	}
 
-		return $fn;
+	static function delete($store, $key) {
+		if (!config::$fcache_dir) {
+			return;
+		}
+		$fn = self::filename($store, $key);
+		@unlink($fn);
+	}
+
+	static function filename($store, $key) {
+		return config::$fcache_dir.$store.'/'.md5(json_encode($key)).'.s';
 	}
 }
