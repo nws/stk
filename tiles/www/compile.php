30d113262ed0f78a636fef39206928f895cd3d35
huf <huf@keszen.hu>
2014-07-11 22:09:14 +0200

    compile fixes

--- tiles/www/compile.php
+++ tiles/www/compile.php
@@ -7,14 +7,19 @@ $args = t::argv();
 $mode = array_shift($args);
 
 switch ($mode) {
-	case 'scss': 
-		header('content-type: text/css');
-		$target = implode('/',$args);
-		$src = preg_replace('/\.css$/', '.scss',$target);
-		$st = 'scss '.escapeshellarg($src).' '.escapeshellarg($target);
-		exec ($st);
-		print file_get_contents($target);
+	case 'scss':
+		header('Content-Type: text/css');
+		$target = implode('/', $args);
+		$src = preg_replace('/\.css$/', '.scss', $target);
+		$st = 'scss -C '.escapeshellarg($src).' '.escapeshellarg($target);
+		exec($st, $output, $rv);
+		if ($rv !== 0) {
+			debug("SCSS compiler died with $output ($rv)");
+		}
+		else {
+			echo file_get_contents($target);
+		}
 	break;
 	default:
-		die('invalid mode');
+		error('invalid mode: '.$mode);
 }
