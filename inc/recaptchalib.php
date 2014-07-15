390c4d873e5e6220243848d0926bdbd2bd3eb0e4
irajmon <irajmon@github.com>
2014-07-15 20:06:24 +0200

    recaptcha fix

--- inc/recaptchalib.php
+++ inc/recaptchalib.php
@@ -142,6 +142,7 @@ function recaptcha_get_custom_html ($pubkey, $error = null, $use_ssl = false, $h
 		$errorpart = "&amp;error=" . $error;
 	}
 	return '
+KAKAKAAKAAKAKAKAKAKAKAKAAK
 <script type="text/javascript">
 	var RecaptchaOptions = {
 		theme : \'custom\',
@@ -151,22 +152,26 @@ function recaptcha_get_custom_html ($pubkey, $error = null, $use_ssl = false, $h
 
 <div id="'.$html_id.'" style="display:none">
 	<div id="recaptcha_image"></div>
-	<span id="recaptcha_instructions_error">Incorrect, please try again</span>
-	<span class="recaptcha_only_if_image">Type the two words:</span>
+	<div class="recaptcha_only_if_incorrect_sol" style="color:red">Incorrect please try again</div>
+	<span class="recaptcha_only_if_image">Enter the words above:</span>
 	<span class="recaptcha_only_if_audio">Enter the numbers you hear:</span>
 	<input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
-	<div class="recaptcha_reload"><a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a></div>
-	<div class="recaptcha_only_if_image recaptcha_get_audio"><a href="javascript:Recaptcha.switch_type(\'audio\')">Get an audio CAPTCHA</a></div>
-	<div class="recaptcha_only_if_audio recaptcha_get_image"><a href="javascript:Recaptcha.switch_type(\'image\')">Get an image CAPTCHA</a></div>
-	<div class="recaptcha_show_help"><a href="javascript:Recaptcha.showhelp()">Help</a></div>
+	<div><a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a></div>
+	<div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type(\'audio\')">Get an audio CAPTCHA</a></div>
+	<div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type(\'image\')">Get an image CAPTCHA</a></div>
+	<div><a href="javascript:Recaptcha.showhelp()">Help</a></div>
 </div>
 
-<script type="text/javascript" src="'.$server.'/challenge?k='.$pubkey.'"></script>
-
+<script type="text/javascript"
+src="http://www.google.com/recaptcha/api/challenge?k='.$pubkey.'">
+</script>
 <noscript>
-	<iframe src="'.$server.'/noscript?k='.$pubkey.'" height="300" width="500" frameborder="0"></iframe><br>
-	<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-	<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+<iframe src="http://www.google.com/recaptcha/api/noscript?k='.$pubkey.'"
+height="300" width="500" frameborder="0"></iframe><br>
+<textarea name="recaptcha_challenge_field" rows="3" cols="40">
+</textarea>
+<input type="hidden" name="recaptcha_response_field"
+value="manual_challenge">
 </noscript>
 ';
 }
