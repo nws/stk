c4a8cc89a99735d4e9b48d803441c707e715f37f
ivo rajmon <ivo.rajmon@gmail.com>
2014-07-21 14:03:32 +0200

    compiler- added some debugging

--- tiles/www/compile.php
+++ tiles/www/compile.php
@@ -8,16 +8,18 @@ $mode = array_shift($args);
 
 switch ($mode) {
 	case 'scss':
-		header('Content-Type: text/css');
 		$target = implode('/', $args);
 		$src = preg_replace('/\.css$/', '.scss', $target);
 		$st = 'scss -C '.escapeshellarg($src).' '.escapeshellarg($target).' 2>&1';
 		exec($st, $output, $rv);
 		if ($rv !== 0) {
+			header('Content-Type: text/plain');
 			debug("SCSS compiler died with ".print_r($output,1)." ($rv)");
 			die("SCSS compiler died with ".print_r($output,1)." ($rv)");
-		}
-		else {
+		} elseif (isset($_GET['debug'])) {
+			header('Content-Type: text/plain');
+			print file_get_contents($target);
+		} else {
 			$args[0].='_00'.time().rand(0,1000);
 			$target = implode('/', $args);
 			redirect($target);
